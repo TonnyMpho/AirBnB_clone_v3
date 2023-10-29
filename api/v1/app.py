b9ec6c356716d7454e801265e43abece2ee9a5cb
TonnyMpho <tonnympho1@gmail.com>
2023-10-29 11:11:07 -0700

    AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ AirBnB clone - RESTful API """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown(e):
     """ close the storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """ handler for 404 errors """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
