d8f56033196f2ecfb0f0a19bead8afa379c4a6d5
TonnyMpho <tonnympho1@gmail.com>
2023-10-30 11:36:53 -0700

    AirBnB clone - RESTful API

---
+++ api/v1/views/cities.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+""" view for State objects that handles all default RESTFul API actions """
+from flask import request, jsonify, abort
+from api.v1.views import app_views
+from models.state import State
+from models.city import City
+from models import storage
+
+methods = ["GET", "POST", "DELETE"]
+
+
+@app_views.route(
+        "/states/<state_id>/cities", strict_slashes=False, methods=["GET"])
+def states(state_id=None):
+    """ handles all default RESTFul API actions """
+    if state_id:
+        state = storage.get(State, state_id)
+
+        if not state:
+            abort(404)
+        return jsonify([city.to_dict() for city in state.cities])
+
+
+@app_views.route(
+        "/states/<state_id>/cities", methods=["POST"], strict_slashes=False)
+def create_city(state_id):
+    """ method that handles POST request and craetes a city object """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    instance_data = request.get_json()
+    if not instance_data:
+        return jsonify({"error": "Not a JSON"}, 400)
+    if "name" not in instance_data:
+        return jsonify({"error": "Missing name"}, 400)
+
+    instance_data["state_id"] = state_id
+    city = City(**instance_data)
+    city.save()
+    return jsonify(city.to_dict()), 201
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=methods)
+def cities(city_id):
+    """ City objects that handles GET actions """
+    if request.method == "GET":
+        if city_id:
+            city = storage.get(City, city_id)
+
+            if not city:
+                abort(404)
+            return jsonify(city.to_dict())
+
+    if request.method == "DELETE":
+        if city_id:
+            city = storage.get(City, city_id)
+
+            if not city:
+                abort(404)
+            storage.delete(city)
+            storage.save()
+            return jsonify({}), 200
+
+    if request.method == "PUT":
+        if city_id:
+            city = storage.get(City, city_id)
+            update_data = request_json()
+
+            if not city:
+                abort(404)
+            if not update_data:
+                return jsonify("error": "Not a JSON"), 400
+
+            for key, value in update_data.items():
+                if key not in ["id", "state_id", "created_at", "updated_at"]:
+                    setattr(city, key, value)
+            city.save()
+            return jsonify(city.to_dict()), 200
