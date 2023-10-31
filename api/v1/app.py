74c328f4799b3cb03c48c738c5ae7a561927ef09
TonnyMpho <tonnympho1@gmail.com>
2023-10-30 19:03:14 -0700

    AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ AirBnB clone - RESTful API """
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -23,7 +25,7 @@ def not_found(e):
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST")
-    port = os.getenv("HBNB_API_PORT")
+    port = int(os.getenv("HBNB_API_PORT"))
 
     if not host or not port:
         host = "0.0.0.0"
